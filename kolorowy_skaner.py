a824881bcd862da507513bdc79b70440d285ea72
Jagoda Flejmer <jflejmer@student.agh.edu.pl>
2025-03-24 16:05:55 +0100

    dodane funkcje i komentarze, tablice jeszcze nie działające

--- kolorowy_skaner.py
+++ kolorowy_skaner.py
@@ -1,7 +1,8 @@
-KEYWORDS = ["if", "else", "elif", "for", "while"]
-BOOLEANS = ["True", "False"]
+KEYWORDS = ["if", "else", "elif", "for", "while", "import", "function", "return"]
+BOOLEANS = ["true", "false"]
 OPERATORS = ["=", "!", "+", "-", "*", "/", "<", ">"]
 BRACKETS = ["(", ")", "[", "]", "{", "}"]
+TYPES = ["int", "string", "decimal", "true", "false"]
 
 def is_letter(c):
     return c.isalpha() or c == "_"
@@ -46,17 +47,43 @@ def color_lines(line):
             text = line[start_of_identifier:i]
             if text in KEYWORDS:
                 result += f'<span class="token-keyword">{text}</span>'
+            elif text in BOOLEANS:
+                result += f'<span class="token-boolean">{text}</span>'
+            elif text in TYPES:
+                result += f'<span class="token-type">{text}</span>'
             else:
                 result += f'<span class="token-identifier">{text}</span>'
             continue
+
+
         if c in OPERATORS:
             i += 1
             result += f'<span class="token-operator">{c}</span>'
             continue
+
         if c in BRACKETS:
             i += 1
             result += f'<span class="token-bracket">{c}</span>'
             continue
+
+        if c == "#":
+            result += f'<span class="token-comment">{line[i:]}</span>'
+            break
+
+        if c == "[":
+            start_of_array = i
+            i += 1
+            while i < l and (is_letter_or_digit(line[i]) or line[i] == " " or line[i] == ","):
+                i += 1
+            if i < l and line[i] == "]":
+                result += f'<span class="token-array">{line[start_of_array:i+1]}</span>'
+                continue
+
+        if c == "=" and i > 0 and line[i - 1] == "]":
+            result += f'<span class="token-operator">=</span>'
+            i += 1
+            continue
+
         result += c
         i += 1
     return result
@@ -73,6 +100,9 @@ def color_file(input_file, output_file):
         f.write('.token-keyword { color: #8B008B; }\n')
         f.write('.token-operator { color: #C25283; }\n')
         f.write('.token-bracket { color: #E38AAE; }\n')
+        f.write('.token-type { color: #202071; }\n')
+        f.write('.token-comment { color: #946fbc; }\n')
+        f.write('.token-array { color: #190d5c; }\n')
         f.write('</style></head><body><pre>\n')
 
         for line in lines:
@@ -81,4 +111,4 @@ def color_file(input_file, output_file):
         f.write('\n</pre></body></html>\n')
 
 
-color_file("input_file.py", "result.html")
+color_file("input_file.txt", "result.html")
